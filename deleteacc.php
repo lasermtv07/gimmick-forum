80824c926ba1d0db766603771d1690e91ee6c672
misa_laserm <118477750+lasermtv07@users.noreply.github.com>
2024-04-23 14:49:07 +0200

    muze mazat ucty

---
+++ deleteacc.php
@@ -0,0 +1,18 @@
+<?php
+include "com.php";
+session_start();
+if(isset($_SESSION["jm"])){
+    $f=file_get_contents("acc.txt");
+    $f=explode("\n",$f);
+    $o="";
+    foreach($f as $i){
+        if(explode(";",$i)[0]!==$_SESSION["jm"]){
+            $o.=$i;
+        }
+    }
+    unset($_SESSION["jm"]);
+    file_put_contents("acc.txt",$o);
+}
+
+header("location: .");
+?>
