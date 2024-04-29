e1e03f5db0295b85128ed599f2309e1f8ccedf80
lasermtv07 <laserman.minecraft@seznam.cz>
2024-04-29 21:11:29 +0200

    pred smazanim uctu se zepta na heslo

--- deleteacc.php
+++ deleteacc.php
@@ -1,14 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>delete account?</title>
+</head>
+<body>
 <?php
 include "com.php";
 session_start();
-//TODO: check password
+?>
+<h1>delete account</h1>
+<p>Hello <?php echo $_SESSION["jm"]; ?>, are you sure you want to delete your account?
+If yes, please type you password below:</p>
+<form method=POST>
+<b>Password:</b> <input type=password name=he />
+<br /> <input type=submit name=s />
+<?php
+if(!isset($_POST["s"])){
+	die();
+}
+if(!isUnique(queryLs("acc.txt"),$_SESSION["jm"])){
+	$l=queryLs("acc.txt");
+	if(base64_decode($l[$_SESSION["jm"]]["he"])!==$_POST["he"]){
+		echo "<b>Error: Bad Credentials</b>";
+		die();
+	}
+}
+else {
+	echo "<b>Error: Account doesnt exist</b>";
+	die();
+}
 if(isset($_SESSION["jm"])){
     $f=file_get_contents("acc.txt");
     $f=explode("\n",$f);
     $o="";
     foreach($f as $i){
         if(explode(";",$i)[0]!==$_SESSION["jm"]){
-            $o.=$i;
+            $o.="$i\n";
         }
     }
     unset($_SESSION["jm"]);
@@ -17,3 +44,5 @@ if(isset($_SESSION["jm"])){
 
 header("location: .");
 ?>
+</body>
+</html>
